2660742714032de74fc6e181e937601bf7681f58
HunorZalan <nagyhunorzalan@yahoo.com>
2021-11-03 18:45:50 +0200

    Start

---
+++ ToDoWPF/ToDoWPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ToDoWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
